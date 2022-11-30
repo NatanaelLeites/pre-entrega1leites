ff09cd3295026d2383515f4a6a3c83432acab09b
natanael leites <nathaxx09@hotmail.com>
2022-11-29 21:34:04 -0300

    sin-terminar

---
+++ java-script.js
@@ -0,0 +1,14 @@
+let nombre = prompt('Ingrese su nombre')
+let muaythai = (900)
+let boxeo = (900)
+let mma = (900)
+let libre = (2100)
+
+
+alert("Hola!" + " " + nombre + " " + "Bienvenid@ a FightFit Montevideo")
+
+alert("Sigamos")
+
+
+
+
